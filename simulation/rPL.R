fcfced1dfe55a9e33c0c987872ba0f7dc07cee64
shuxiaoc <shuxiaoc@gmail.com>
2016-06-22 19:00:37 -0400

    alterations

--- simulation/rPL.R
+++ simulation/rPL.R
@@ -2,15 +2,14 @@ library(FAdist) #for sampling from Gumbel distribution
 #this sampler is based on the equivalence
 #of Thurstonian model and Luce's Choice
 #when utilities are drawn independently
-plSampler = function(ability, beta = 2){
-  #ability is the vector of parameters in Plackett-Luce model, 
-  #and is normalized if not sum to 1;
-  #beta is the scale parameter of Gumbel distribution.
-  nitem = length(ability)
-  #normalize
-  ability = ability / sum(ability)
+rPL = function(S, beta = 2){
+  #S: score vector 
+  #beta: arbitrary positive scale parameter
+  nitem = length(S)
+  
   #compute the location parameter of Gumbel distribution
-  mu = beta * log(ability)
+  mu = beta * (S - log(sum(exp(S))))
+  
   #sample from Gumbell distribution
   utility = rgumbel(nitem, scale = beta, location = mu)
   names(utility) = 1:nitem #assign labels to items
