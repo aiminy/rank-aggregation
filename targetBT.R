192d983f907c5e462576b0027658f21f1e8694d6
shuxiaoc <shuxiaoc@gmail.com>
2016-06-24 18:54:34 -0400

    Helper function for sgdBT

--- targetBT.R
+++ targetBT.R
@@ -1,4 +1,5 @@
 #calculate the target function to minimize in the B-T model
+#as well as the gradient for each observation (in Jacobian-like style)
 targetBT = function(score, data, mu, sigma){
   #browser()
   
@@ -15,7 +16,11 @@ targetBT = function(score, data, mu, sigma){
   colnames(data) = 1:nvar #assign labels to varieties
   
   #initialize, add the normal term
-  res = as.numeric(0.5 * (t(score - mu) %*% solve(sigma) %*% (score - mu)))
+  inv_sigma = solve(sigma)
+  target_value = as.numeric(0.5 * (t(score - mu) %*% inv_sigma %*% (score - mu)))
+  #J-matrix, each row is the gradient
+  J = matrix(0, nobs, nvar)
+  
   
   #loop over all observations
   for(i in 1:nobs){
@@ -27,20 +32,35 @@ targetBT = function(score, data, mu, sigma){
     #the length of i-th observation
     nrank = length(ranks)
     
-    #calculate likelihood
+    #initialize the gradient
+    gradient = 1 / nobs * inv_sigma %*% (score - mu)
+    
+    
     for(j in 1:(nrank - 1)){
       
       for(k in (j + 1):nrank){
         
-        res = res + log(1 + exp(-score[ranking[j]] + score[ranking[k]]))
+        #ranking[j] wins over ranking[k]
+        win = ranking[j]
+        lose = ranking[k]
+        exp_term = exp(-score[win] + score[lose])
         
+        #update the value of the target function
+        target_value = target_value + log(1 + exp_term)
+        
+        #update gradient
+        gradient[win] = gradient[win] - exp_term / (1 + exp_term)
+        gradient[lose] = gradient[lose] + exp_term / (1 + exp_term)
+          
       }
       
     }
+    
+    J[i, ] = gradient
+    
+    
   }
   
-  return(res)
+  return(list(value = target_value, Jacobian = J))
 }
-
-
 
